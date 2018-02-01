eafda54d222a3892e59cfbd09fb673fa6a34b830
braxaxaxaxaxa <35474625+belphkatekyo@users.noreply.github.com>
2018-02-01 19:01:19 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Networking</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src= "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+<div class = "mainBase">
+	<div class = "centerDiv">
+		<!--Second Layer | Pager-->
+		<div class = "headerDiv">
+			<table>
+				<tr>
+					<td>
+						<img style="position: relative;left:0px;top:0px;padding: 0px;" src="networking.jpg" width="100%px" height="100p%">
+					</td>
+				</tr>
+			</table>
+		</div>
+		<!--Menu Item and Search Bar-->
+		<div class = "menuItem">
+			<a style="float:left"; class = "linker" href="About.php" id = "about">About</a>
+			<a style="float:left"; class = "linker" href="cabling.php" id = "profile">Cabling Types</a>
+			<a style="float:left"; class = "linker" href="fiber.php" id = "contact" href="">Fiber Optics Connectors</a>
+			<a style="float:left"; class = "linker" href="packet.php" id = "signup" href="">-Packet Tracers-</a>
+		</div>
+		
+	</div>
+	<img src="blugo1.png" width="100%" height="100%">
+	<!--Login Base-->
