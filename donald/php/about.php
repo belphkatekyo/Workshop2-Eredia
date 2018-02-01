ace2ae5cde5463eb2826bd23b7f8a16b565ae27e
braxaxaxaxaxa <35474625+belphkatekyo@users.noreply.github.com>
2018-02-01 13:57:52 +0800

    Add files via upload

---
+++ donald/php/about.php
@@ -0,0 +1,94 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<script type="text/javascript" src= "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+<div class = "mainBase">
+	<div class = "centerDiv">
+		<!--Second Layer | Pager-->
+		<div class = "headerDiv">
+				<table>
+				<tr>
+					<td><img style="position: relative; top: 0px;margin: 0px;padding: 0px;" src="../img/aboutlogo1.png" width="100px" height="90px"></td>
+					<td>
+						<img style="position: relative;top:0px;padding: 0px;" src="../img/about1.png" width="590px" height="100px">
+					</td>
+				</tr>
+			</table>
+		</div>
+		<!--Menu Item and Search Bar-->
+		<div class = "menuItem">
+			<a style="float:left"; class = "linker" href="../index.php" id = "Home" href=""> Home </a>
+		</div>
+		<img src="../img/supercomputers.jpg" width="100%" height="250px">
+			<div id = "menu">
+				<table>
+				<tr>
+				<p style="color: white" class = "topic" >Network Hardware and Peripherals</p>
+					
+							</tr>
+
+					<tr>
+						<td><img src="../img/hub.gif" width="200px" height="auto"></td>
+						<td>
+							<p style="color: white" class = "foodName" >Switch/HUB</p>
+							<p style = "color: white;"
+							>
+								An Ethernet hub, active hub, network hub, repeater hub, multiport repeater, or simply hub is a network hardware device for connecting multiple Ethernet devices together and making them act as a single network segment. It has multiple input/output (I/O) ports, in which a signal introduced at the input of any port appears at the output of every port except the original incoming.[1] A hub works at the physical layer (layer 1) of the OSI model.[2] A repeater hub also participates in collision detection, forwarding a jam signal to all ports if it detects a collision. In addition to standard 8P8C ("RJ45") ports, some hubs may also come with a BNC or an Attachment Unit Interface (AUI) connector to allow connection to legacy 10BASE2 or 10BASE5 network segments.</p>
+								<p style = "color: white;">
+								FUNCTION:
+									It is aware of physical layer packets, that is it can detect their start (preamble), an idle line (interpacket gap) and sense a collision which it also propagates by sending a jam signal. A hub cannot further examine or manage any of the traffic that comes through it: any packet entering any port is rebroadcast on all other ports.[6] A hub/repeater has no memory to store any data in – a packet must be transmitted while it is received or is lost when a collision occurs (the sender should detect this and retry the transmission)</p>
+						</td>
+					</tr>
+					<tr>
+						<td><img src="../img/utp.jpg" width="200px" height="auto"></td>
+						<td>
+							<p style="color: white" class = "foodName" >UTP Cable</p>
+							<p style = "color: white;">
+								Twisted pair cabling is a type of wiring in which two conductors of a single circuit are twisted together for the purposes of canceling out electromagnetic interference (EMI) from external sources; for instance, electromagnetic radiation from unshielded twisted pair (UTP) cables, and crosstalk between neighboring pairs. It was invented by Alexander Graham Bell.
+							</p>
+							<p style = "color: white;">
+								FUNCTION:
+									The twist rate (also called pitch of the twist, usually defined in twists per meter) makes up part of the specification for a given type of cable. When nearby pairs have equal twist rates, the same conductors of the different pairs may repeatedly lie next to each other, partially undoing the benefits of differential mode. For this reason it is commonly specified that, at least for cables containing small numbers of pairs, the twist rates must differ.[2]
+
+In contrast to shielded or foiled twisted pair (typically F/UTP or S/FTP cable shielding), UTP (unshielded twisted pair) cable is not surrounded by any shielding. UTP is the primary wire type for telephone usage and is very common for computer networking, especially as patch cables or temporary network connections due to the high flexibility of the cables.
+							</p>
+						</td>
+					</tr>
+					<tr>
+						<td><img src="../img/rj45.jpg" width="200px" height="auto"></td>
+						<td>
+							<p style="color: white" class = "foodName" >RJ-45</p>
+							<p style = "color: white;">
+								A Registered Jack (RJ) is a standardized telecommunication network interface for connecting voice and data equipment to a service provided by a local exchange carrier or long distance carrier. 
+							</p>
+							<p style = "color: white;">
+							FUNTION:
+								Registered Jack refers to both the female physical connector (modular connector) and its wiring, but the term is often used loosely to refer to modular connectors regardless of wiring or gender, such as in Ethernet over twisted pair. There is much confusion over these connection standards. The same six-position plug and jack commonly used for telephone line connections may be used for RJ11, RJ14 or even RJ25, all of which are names of interface standards that use this physical connector. The RJ11 standard dictates a single wire pair connection, while RJ14 is a configuration for two lines, and RJ25 uses all six wires for three telephones lines. The RJ designations, though, only pertain to the wiring of the jack, hence the name Registered Jack; it is commonplace, but not strictly correct, to refer to an unwired plug by any of these names.</p>
+						</td>
+					</tr>
+					<tr>
+						<td><img src="../img/crimper.jpg" width="200px" height="auto"></td>
+						<td>
+							<p style="color: white" class = "foodName" >Crimper Tool</p>
+							<p style = "color: white;">
+								The metals are joined together via a special connector. Stripped wire (often stranded) is inserted through the correctly sized opening of the connector, and a crimper is used to tightly squeeze the opening against the wire. Depending on the type of connector used, it may be attached to a metal plate by a separate screw or bolt or it could be simply screwed on using the connector itself to make the attachment like an F connector..	</p>
+							<p style = "color: white;">
+								FUNCTION:
+									Crimping is most extensively used in metalworking. Crimping is commonly used to fix bullets in their cartridge cases, for rapid but lasting electrical connections, securing lids on metal food cans, and many other applications. Because it can be a cold-working technique, crimping can also be used to form a strong bond between the workpiece and a non-metallic component.
+							</p>
+						</td>
+					</tr>
+				</table>
+			</div>
+	</div>
+	<img src="../img/blugo.png" width="100%" height="100%">
+			</div>
+	</div>
+</div>
+</body>
+</html>
