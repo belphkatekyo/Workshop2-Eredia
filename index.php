2d7abe2bbda7ed8cf5cf1d72d26e2fdb8a5ee869
braxaxaxaxaxa <35474625+belphkatekyo@users.noreply.github.com>
2018-02-01 13:38:55 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Networking</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src= "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+<div class = "mainBase">
+	<div class = "centerDiv">
+		<!--Second Layer | Pager-->
+		<div class = "headerDiv">
+			<table>
+				<tr>
+					<td><img style="position: relative; top: 0px;margin: 0px;padding: 0px;" src="img/homelogo.png" width="100px" height="90px"></td>
+					<td>
+						<img style="position: relative;top:0px;padding: 0px;" src="img/networking.png" width="590px" height="100px">
+					</td>
+				</tr>
+			</table>
+		</div>
+		<!--Menu Item and Search Bar-->
+		<div class = "menuItem">
+			<a style="float:left"; class = "linker" href="php/About.php" id = "about">About</a>
+			<a style="float:left"; class = "linker" href="php/cabling.php" id = "profile">Cabling Types</a>
+			<a style="float:left"; class = "linker" href="php/fiber.php" id = "contact" href="">Fiber Optics Connectors</a>
+			<a style="float:left"; class = "linker" href="php/packet.php" id = "signup" href="">-Packet Tracers-</a>
+		</div>
+		
+	</div>
+	<img src="img/blugo1.png" width="100%" height="100%">
+	<!--Login Base-->
